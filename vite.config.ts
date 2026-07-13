2e354f49c6fd695983c6c898db1e8b11ed1a1f0d
Ibrahim-svmb <ibrahim.svmb@gmail.com>
2026-07-13 16:39:26 +0000

    fix: update config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,36 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/auth': {
+        target: 'http://localhost:8765',
+        changeOrigin: true
+      },
+      '/users': {
+        target: 'http://localhost:8765',
+        changeOrigin: true
+      },
+      '/pricing': {
+        target: 'http://localhost:8765',
+        changeOrigin: true
+      },
+      '/tracking': {
+        target: 'http://localhost:8765',
+        changeOrigin: true
+      },
+      '/transactions': {
+        target: 'http://localhost:8765',
+        changeOrigin: true
+      },
+      '/accounts': {
+        target: 'http://localhost:8765',
+        changeOrigin: true
+      },
+      '/personnalisation': {
+        target: 'http://localhost:8765',
+        changeOrigin: true
+      }
+    }
+  }
 })
