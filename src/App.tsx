3e5d1aad3fe00777185be65e05b9d23cd0ebf034
Ibrahim-svmb <ibrahim.svmb@gmail.com>
2026-07-16 08:10:37 +0000

    fix: remove app css file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
 import { Login } from './components/Login';
 import { DashboardOverview } from './components/DashboardOverview';
 import { ClientManager } from './components/ClientManager';
